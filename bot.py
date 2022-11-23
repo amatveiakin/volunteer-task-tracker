bd3a547dcb1d96dd6449a5e324eae9adf53b8514
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-11-23 10:39:21 +0400

    Task creation bot stub

--- bot.py
+++ bot.py
@@ -1,113 +1,187 @@
 import logging
 
-from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram import Update, User, Chat, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+
 
 logger = logging.getLogger(__name__)
 
-# Store bot screaming status
-screaming = False
+class Task:
+    SHELTER = "SHELTER"
+    TRANSPORT = "TRANSPORT"
+    VOLUNTEER = "VOLUNTEER"
+    QUESTION = "QUESTION"
+    OTHER = "OTHER"
 
-# Pre-assign menu text
-FIRST_MENU = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button."
-SECOND_MENU = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons."
+class Action:
+    CANCEL = "CANCEL"
+    SELECT = "SELECT"
+    CREATE = "CREATE"
 
-# Pre-assign button text
-NEXT_BUTTON = "Next"
-BACK_BUTTON = "Back"
-TUTORIAL_BUTTON = "Tutorial"
+def action_select(kind: str) -> str:
+    return "/".join([Action.SELECT, kind])
 
-# Build keyboards
-FIRST_MENU_MARKUP = InlineKeyboardMarkup([[
-    InlineKeyboardButton(NEXT_BUTTON, callback_data=NEXT_BUTTON)
-]])
-SECOND_MENU_MARKUP = InlineKeyboardMarkup([
-    [InlineKeyboardButton(BACK_BUTTON, callback_data=BACK_BUTTON)],
-    [InlineKeyboardButton(TUTORIAL_BUTTON, url="https://core.telegram.org/bots/api")]
+class NewTaskState:
+    def __init__(self):
+        self.kind: str|None = None
+        self.text: str|None = None
+
+# The kind of the task this user is currently creating.
+# TODO:
+#   - save to persistent storage,
+#   - or find a way to extract this info from chat (akin to callback_data),
+#   - or at least add a way to monitor the dict, so that we can only restart the bot when it's empty.
+new_task_states: dict[int, NewTaskState] = {}
+
+NEW_TASK_INPUT_TEXT = {}
+NEW_TASK_CONFIRMATION_TEXT = {}
+NEW_TASK_CREATED_TEXT = {}
+
+NEW_TASK_KIND_SELECTION_TEXT = """<b>Create new task</b>\n
+Choose the type of the task."""
+CANCELLED_TEXT = "Cancelled."
+
+NEW_TASK_INPUT_TEXT[Task.SHELTER] = """<b>⛺️ Looking for shelter</b>\n
+Enter you request below. Make sure to include #LocationHashtag."""
+NEW_TASK_CONFIRMATION_TEXT[Task.SHELTER] = """<b>⛺️ You are about to submit the following shelter request:</b>"""
+NEW_TASK_CREATED_TEXT[Task.SHELTER] = """<b>⛺️ Shelter request created:</b>"""
+
+NEW_TASK_INPUT_TEXT[Task.TRANSPORT] = """<b>🚗 Looking for transport</b>\n
+Enter you request below. Make sure to include #LocationHashtag for the source and destination locations."""
+NEW_TASK_CONFIRMATION_TEXT[Task.TRANSPORT] = """<b>🚗 You are about to submit the following transport request:</b>"""
+NEW_TASK_CREATED_TEXT[Task.TRANSPORT] = """<b>🚗 Transport request created:</b>"""
+
+# # TODO: Add info wiki links
+# NEW_TASK_INPUT_TEXT[Task.QUESTION] = """<b>❔ Ask a question</b>\n
+# Make sure to <i>check the wiki first!</i>
+# If you cannot find an answer elsewhere, please enter the question below"""
+# NEW_TASK_CONFIRMATION_TEXT[Task.QUESTION] = """You are about to submit the following question:"""
+
+# TODO: Privacy policy
+NEW_TASK_CONFIRMATION_TEMPLATE = """{header}\n
+{task}\n
+Your telegram account ({user}) will be displayed publicly. Community members will use it to reach you.
+Please make sure to mark this task as “Done” when your question is answered."""
+# TODO: Tell the user how to get task status
+NEW_TASK_CREATED_TEMPLATE = """{header}\n
+{task}"""
+
+CANCEL_BUTTON = "❌ Cancel"
+CREATE_TASK_BUTTON = "❇️ Create task"
+NEW_SHELTER_BUTTON = "⛺️ Shelter"
+NEW_TRANSPORT_BUTTON = "🚗 Transport"
+NEW_VOLUNTEER_BUTTON = "🙋 Volunteer"
+NEW_QUESTION_BUTTON = "❔ Question"
+NEW_OTHER_BUTTON = "Other"
+
+NEW_TASK_MARKUP = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(NEW_SHELTER_BUTTON, callback_data=action_select(Task.SHELTER)),
+        InlineKeyboardButton(NEW_TRANSPORT_BUTTON, callback_data=action_select(Task.TRANSPORT)),
+    ],
+    # TODO: Implement these task types
+    # [
+    #     InlineKeyboardButton(NEW_VOLUNTEER_BUTTON, callback_data=action_select(Task.VOLUNTEER)),
+    #     InlineKeyboardButton(NEW_QUESTION_BUTTON, callback_data=action_select(Task.QUESTION)),
+    #     InlineKeyboardButton(NEW_OTHER_BUTTON, callback_data=action_select(Task.OTHER)),
+    # ],
+    [
+        InlineKeyboardButton(CANCEL_BUTTON, callback_data=Action.CANCEL),
+    ],
 ])
 
+# TODO: Add "Back" button
+NEW_TASK_INPUT_MARKUP = InlineKeyboardMarkup([[
+    InlineKeyboardButton(CANCEL_BUTTON, callback_data=Action.CANCEL),
+]])
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """
-    This function would be added to the dispatcher as a handler for messages coming from the Bot API
-    """
-
-    # Print to console
-    print(f'{update.message.from_user.first_name} wrote {update.message.text}')
-
-    if screaming and update.message.text:
-        context.bot.send_message(
-            update.message.chat_id,
-            update.message.text.upper(),
-            # To preserve the markdown, we attach entities (bold, italic...)
-            entities=update.message.entities
-        )
-    else:
-        # This is equivalent to forwarding, without the sender's name
-        update.message.copy(update.message.chat_id)
+def new_task_confirmation_markup(task_kind, task_text):
+    # TODO: Add "Edit" button
+    return InlineKeyboardMarkup([[
+        InlineKeyboardButton(CANCEL_BUTTON, callback_data=Action.CANCEL),
+        InlineKeyboardButton(CREATE_TASK_BUTTON, callback_data=Action.CREATE),
+    ]])
 
 
-def scream(update: Update, context: CallbackContext) -> None:
-    """
-    This function handles the /scream command
-    """
-
-    global screaming
-    screaming = True
-
-
-def whisper(update: Update, context: CallbackContext) -> None:
-    """
-    This function handles /whisper command
-    """
-
-    global screaming
-    screaming = False
-
-
-def menu(update: Update, context: CallbackContext) -> None:
-    """
-    This handler sends a menu with the inline buttons we pre-assigned above
-    """
-
+def new_task(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(
         update.message.from_user.id,
-        FIRST_MENU,
+        NEW_TASK_KIND_SELECTION_TEXT,
         parse_mode=ParseMode.HTML,
-        reply_markup=FIRST_MENU_MARKUP
+        reply_markup=NEW_TASK_MARKUP,
     )
 
 
-def button_tap(update: Update, context: CallbackContext) -> None:
+def on_button_tap(update: Update, context: CallbackContext) -> None:
     """
-    This handler processes the inline buttons on the menu
+    This handler processes all inline buttons
     """
 
     data = update.callback_query.data
-    text = ''
-    markup = None
+    update.callback_query.answer()  # stop the client-side loading animation
+    user = update.effective_user  # TODO: Can the user be `None`?
+    chat = update.effective_chat  # TODO: Can the chat be `None`?
+    assert(isinstance(user, User))
+    assert(isinstance(chat, Chat))
 
-    if data == NEXT_BUTTON:
-        text = SECOND_MENU
-        markup = SECOND_MENU_MARKUP
-    elif data == BACK_BUTTON:
-        text = FIRST_MENU
-        markup = FIRST_MENU_MARKUP
+    match data.split("/"):
+        case [Action.CANCEL]:
+            new_task_states.pop(user.id, None)
+            context.bot.send_message(
+                chat.id,
+                CANCELLED_TEXT,
+                ParseMode.HTML,
+            )
+        case [Action.SELECT, task_kind]:
+            new_task_states[user.id] = NewTaskState()
+            new_task_states[user.id].kind = task_kind
+            update.callback_query.message.edit_text(
+                NEW_TASK_INPUT_TEXT[task_kind],
+                ParseMode.HTML,
+                reply_markup=NEW_TASK_INPUT_MARKUP,
+            )
+        case [Action.CREATE]:
+            task_kind = new_task_states[user.id].kind
+            task_text = new_task_states[user.id].text
+            # TODO: Save the task
+            print(f"{task_kind} task created:\n{task_text}")
+            new_task_states.pop(user.id, None)
+            message_text = NEW_TASK_CREATED_TEMPLATE.format(
+                header=NEW_TASK_CREATED_TEXT[task_kind],
+                task=task_text,
+            )
+            context.bot.send_message(
+                chat.id,
+                message_text,
+                ParseMode.HTML,
+            )
 
-    # Close the query to end the client-side loading animation
-    update.callback_query.answer()
-
-    # Update message content with corresponding menu section
-    update.callback_query.message.edit_text(
-        text,
-        ParseMode.HTML,
-        reply_markup=markup
+def on_message(update: Update, context: CallbackContext) -> None:
+    user = update.message.from_user
+    task_kind = new_task_states[user.id].kind
+    if task_kind is None:
+        # TODO: Do something about unexpected messages
+        return
+    task_text = update.message.text  # TODO: Escape (or validate) HTML
+    new_task_states[user.id].text = task_text
+    # TODO: Test that non-username mentions work
+    user_mention = f"@{user.username}" or f'<a href="tg://user?id={user.id}">{user.first_name}</a>'
+    message_text = NEW_TASK_CONFIRMATION_TEMPLATE.format(
+        header=NEW_TASK_CONFIRMATION_TEXT[task_kind],
+        user=user_mention,
+        task=task_text,
+    )
+    context.bot.send_message(
+        update.message.chat_id,
+        message_text,
+        parse_mode=ParseMode.HTML,
+        reply_markup=new_task_confirmation_markup(task_kind, task_text),
     )
 
 
 def main() -> None:
     with open('api_key') as f:
-        token = f.read()
+        token = f.read().strip()
     updater = Updater(token)
 
     # Get the dispatcher to register handlers
@@ -115,15 +189,14 @@ def main() -> None:
     dispatcher = updater.dispatcher
 
     # Register commands
-    dispatcher.add_handler(CommandHandler("scream", scream))
-    dispatcher.add_handler(CommandHandler("whisper", whisper))
-    dispatcher.add_handler(CommandHandler("menu", menu))
+    # TODO: Add a button for this
+    dispatcher.add_handler(CommandHandler("newtask", new_task))
 
     # Register handler for inline buttons
-    dispatcher.add_handler(CallbackQueryHandler(button_tap))
+    dispatcher.add_handler(CallbackQueryHandler(on_button_tap))
 
-    # Echo any message that is not a command
-    dispatcher.add_handler(MessageHandler(~Filters.command, echo))
+    # Register handler for text input (except commands)
+    dispatcher.add_handler(MessageHandler(~Filters.command, on_message))
 
     # Start the Bot
     updater.start_polling()
